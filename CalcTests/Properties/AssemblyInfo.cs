044bc7ee2508c4c7b08d600a8fff5f64c18e9662
Dmytro <pempusdima51@gmail.com>
2024-10-07 21:22:52 +0300

    Add project files.

---
+++ CalcTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f31bbe0-3559-43c8-a167-93419e93676e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+using CalcClassBr;
+using System;
+namespace CalcTests.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("System.Data.SqlClient", @"Data Source=DESKTOP-AOVML30\SQLEXPRESS;Initial Catalog=CalcTestsBD;Integrated Security=True;", "TestCases", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestBrackets()
+        {
+            string brackets = (string)TestContext.DataRow["Expression"];
+            AnalaizerClass.expression = brackets;
+            string expected = (string)TestContext.DataRow["ExpectedResult"];
+            string actual;
+            actual = AnalaizerClass.Format();
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
